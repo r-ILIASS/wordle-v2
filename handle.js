b419bd1534c792aea5902efe372c920889e25762
r-ILIASS <iliass.root@gmail.com>
2022-04-03 17:24:40 +0000

    feat[handle.js]: handle adding and deleting data from the tiles

--- handle.js
+++ handle.js
@@ -1,3 +1,7 @@
+const WORD_LENGTH = 5;
+const guessGrid = document.querySelector("[data-guess-grid]");
+
+// event handlers
 const click = (e) => {
   if (e.target.matches("[data-key]")) {
     pressKey(e.target.dataset.key);
@@ -16,8 +20,6 @@ const click = (e) => {
 };
 
 const keydown = (e) => {
-  console.log("keydown line 1");
-
   if (e.key === "Enter") {
     submitGuess();
     return;
@@ -34,8 +36,16 @@ const keydown = (e) => {
   }
 };
 
+// Actions
 const pressKey = (key) => {
-  console.log("pressKey()", key);
+  const activeTiles = getActiveTiles();
+  if (activeTiles.length === WORD_LENGTH) return;
+
+  const nextTile = guessGrid.querySelector(':not([data-state="active"])');
+  nextTile.dataset.state = "active";
+  nextTile.dataset.letter = key.toLowerCase();
+  nextTile.textContent = key;
+  return;
 };
 
 const submitGuess = () => {
@@ -43,9 +53,23 @@ const submitGuess = () => {
 };
 
 const deleteKey = () => {
-  console.log("deleteKey()");
+  const activeTiles = getActiveTiles();
+  const lastTile = activeTiles[activeTiles.length - 1];
+
+  if (activeTiles.length === 0) return;
+
+  lastTile.textContent = "";
+  delete lastTile.dataset.letter;
+  delete lastTile.dataset.state;
+  return;
 };
 
+// helper functions
+const getActiveTiles = () => {
+  return guessGrid.querySelectorAll('[data-state="active"');
+};
+
+// exports
 const handle = {
   click,
   keydown,
